98cb23ec09531430a1c113ed849583399fc75480
Ev Sivkova <ev.sivkova@outlook.com>
2021-02-20 11:14:56 -0800

    modified Calc.php

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -5,42 +5,52 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
+function getCalc($item, $numb1, $numb2)
+{
+    $rightAnswer = 0;
+    switch ($item) {
+        case '+':
+            $rightAnswer = $numb1 + $numb2;
+            break;
+        case '-':
+            $rightAnswer = $numb1 - $numb2;
+            break;
+        case '*':
+            $rightAnswer = $numb1 * $numb2;
+            break;
+        default:
+            break;
+    };
+    return $rightAnswer;
+}
+
 function brainCalc()
 {
     $name = welcome();
 
     $counter = 0;
+    $i = 1;
+
     $operators = ['+', '-', '*'];
 
-    foreach ($operators as $item) {
+    while ($i <= 3) {
         $numb1 = random_int(1, 10);
         $numb2 = random_int(1, 10);
-        $rightAnswer = 0;
-//Calculating correct answer
-        switch ($item) {
-            case '+':
-                $rightAnswer = $numb1 + $numb2;
-                break;
-            case '-':
-                $rightAnswer = $numb1 - $numb2;
-                break;
-            case '*':
-                $rightAnswer = $numb1 * $numb2;
-                break;
-            default:
-                break;
-        };
-//Question
+        $randIndex = array_rand($operators);
+        $item = $operators[$randIndex];
+        $rightAnswer = getCalc($item, $numb1, $numb2);
+        //Question
         line('What is the result of the expression?');
         line("Question: {$numb1} {$item} {$numb2}");
         $answer = prompt('Your answer');
-//Checking
+        //Checking
         if ($rightAnswer === (int)$answer) {
             echo("Correct!\n");
             $counter += 1;
         } else {
             echo("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.\nLet's try again, {$name}!\n");
-        }
+        };
+        $i += 1;
     };
 
     congratulations($counter, $name);
