dbcad3539173f67dd2877968d661ddbacc2c5c79
Ev Sivkova <ev.sivkova@outlook.com>
2021-03-05 08:51:47 -0800

    use function

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace Brain\Games\Even;
 
-use function cli\line;
-use function cli\prompt;
+use function Brain\Games\Engine\playGame;
 
 function getEven(): callable
 {
